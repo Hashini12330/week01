3b3ca6e476ca1d693d006c5fa7be3a5c2c3f8870
Hashini12330 <hashiniperera20007@gmail.com>
2026-09-22 01:35:51 +0530

    2D array

--- Main.java
+++ Main.java
@@ -7,9 +7,34 @@ public class Main {
         System.out.println("Set new name to: " + mons.setName("BOB"));
         
 
+        // print 2D array
+        array arr = new array();
+        arr.printArray();
+
 
     }
+}
 
 
+// 2D array
+class array{
+    int[][] PlayerInv = {
+        {1, 0},
+        {0, 1},
+        {1, 1}
+    };
+
+
+    public void printArray(){
+        for (int i = 0; i < PlayerInv.length; i++){
+            
+            System.out.println("Row " + (i + 1) + ":");
+
+
+            for (int item = 0; item < PlayerInv[i].length; item++){
+                System.out.println("Item" + (item + 1) + ": " + PlayerInv[i][item]);
+            }
+        }
+    }
 
 }
